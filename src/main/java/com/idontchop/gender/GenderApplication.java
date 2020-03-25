0e9f8dd97b0a891714fe58ea464631f12d34ef87
Nate D <nate@idontchop.com>
2020-03-24 21:17:34 -0700

    Application comments

--- src/main/java/com/idontchop/gender/GenderApplication.java
+++ src/main/java/com/idontchop/gender/GenderApplication.java
@@ -3,6 +3,16 @@ package com.idontchop.gender;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Gender Microservice for storage of gender preferences.
+ * 
+ * See MainController for endpoints
+ * 
+ * Spring Data Rest endpoints: /data
+ * 
+ * @author micro
+ *
+ */
 @SpringBootApplication
 public class GenderApplication {
 
