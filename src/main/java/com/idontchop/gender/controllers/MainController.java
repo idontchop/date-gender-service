08561d76f51b857e0608ef2e5a28485989ac39f8
Nate <nate@idontchop.com>
2020-04-10 16:08:17 -0700

    RestMessage, HelloWorld

--- src/main/java/com/idontchop/gender/controllers/MainController.java
+++ src/main/java/com/idontchop/gender/controllers/MainController.java
@@ -1,5 +1,7 @@
 package com.idontchop.gender.controllers;
 
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,6 +9,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Required;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -48,6 +51,12 @@ public class MainController {
 	
 	@Autowired
 	GenderService genderService;
+	
+	@Value ("${server.port}")
+	private String serverPort;
+	
+	@Value("${spring.application.name}")
+	private String appName;
 	
 	/**
 	 * Most of the request to this microservice go here.
@@ -100,6 +109,29 @@ public class MainController {
 	@GetMapping ( value = "/api/user/{username}")
 	public User getUser ( @PathVariable ( name = "username", required=true) String username ) {
 		return genderService.getUser(username);
+	}
+	
+	@GetMapping("/helloWorld")
+	public RestMessage helloWorld () {
+		String serverAddress,serverHost;
+		try {
+			serverAddress = NetworkInterface.getNetworkInterfaces().nextElement()
+					.getInetAddresses().nextElement().getHostAddress();
+		} catch (SocketException e) {
+			serverAddress = e.getMessage();
+		}
+		try {
+			serverHost = NetworkInterface.getNetworkInterfaces().nextElement()
+					.getInetAddresses().nextElement().getHostName();
+		} catch (SocketException e) {
+			serverHost = e.getMessage();
+		}
+		return RestMessage.build("Hello from " + appName)
+				.add("service", appName)
+				.add("host", serverHost)
+				.add("address", serverAddress)
+				.add("port", serverPort);
+			
 	}
 	
 
