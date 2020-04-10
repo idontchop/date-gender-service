80767333324bb9095e8b16829faad35727cd1bc6
Nate <nate@idontchop.com>
2020-04-09 22:00:29 -0700

    update postmapping for reduce

--- src/main/java/com/idontchop/gender/controllers/MainController.java
+++ src/main/java/com/idontchop/gender/controllers/MainController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -59,7 +60,7 @@ public class MainController {
 	 * @param reduceRequest
 	 * @return
 	 */
-	@GetMapping ("/api/reduce")
+	@PostMapping ("/api/reduce")
 	public List<String> reduce( @RequestBody @Valid ReduceRequest reduceRequest) {
 		
 		return genderService.reduceByInterested(reduceRequest.getName(), reduceRequest.getPotentials());
